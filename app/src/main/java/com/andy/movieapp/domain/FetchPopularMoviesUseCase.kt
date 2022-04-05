3ff23267001a1690b966734860f0dce77eb3f428
Andy Villegas <andyvillegas440@gmail.com>
2022-04-05 15:29:26 -0500

    add 'cached movies' on fetch popular movies

--- app/src/main/java/com/andy/movieapp/domain/FetchPopularMoviesUseCase.kt
+++ app/src/main/java/com/andy/movieapp/domain/FetchPopularMoviesUseCase.kt
@@ -1,17 +1,20 @@
 package com.andy.movieapp.domain
 
 import com.andy.movieapp.data.model.Movie
+import com.andy.movieapp.data.model.PaginatedList
 import com.andy.movieapp.data.repository.MoviesRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.util.HashMap
 
 class FetchPopularMoviesUseCase(
     private val moviesRepository: MoviesRepository,
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
 ) {
+    private val moviesCached: HashMap<Int, PaginatedList<Movie>> = hashMapOf()
     suspend operator fun invoke(page: Int = 1): List<Movie> = withContext(defaultDispatcher){
-        val paginatedMovies = moviesRepository.fetchPopularMovies(page)
+        val paginatedMovies = moviesCached.getOrPut(page) { moviesRepository.fetchPopularMovies(page) }
         paginatedMovies.results
     }
 }
